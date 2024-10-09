4c58600a4ffbc04843be6c12b77523f5757b0856
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-10-09 15:19:29 +0900

    [Setting] 프로젝트 생성

---
+++ Moolog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moolog
+//
+//  Created by Jisoo Ham on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
