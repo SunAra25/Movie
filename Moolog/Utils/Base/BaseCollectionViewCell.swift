e9917fd66f8416ababd079d1d14f353faeb701ea
SunAra25 <tjsdkfk77@naver.com>
2024-10-10 19:55:27 +0900

    [Add] BaseCollectionViewCell 구현 #6

---
+++ Moolog/Utils/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  BaseCollectionViewCell.swift
+//  Moolog
+//
+//  Created by 아라 on 10/10/24.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    static var identifier: String {
+        return String(
+            describing: self
+        )
+    }
+    
+    override init(
+        frame: CGRect
+    ) {
+        super.init(
+            frame: frame
+        )
+        
+        setUI()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(
+        coder: NSCoder
+    ) {
+        fatalError(
+            "init(coder:) has not been implemented"
+        )
+    }
+    
+    func setUI() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+}
