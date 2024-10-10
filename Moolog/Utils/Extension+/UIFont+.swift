7070cee8b7622d1113d6c27eb23ab15cd0ea45bf
SunAra25 <tjsdkfk77@naver.com>
2024-10-10 17:45:10 +0900

    [Chore] Constant 파일 컨벤션 적용 #3

--- Moolog/Utils/Extension+/UIFont+.swift
+++ Moolog/Utils/Extension+/UIFont+.swift
@@ -13,17 +13,50 @@ extension UIFont {
         case medium = "Medium"
     }
     
-    static func Pretendard(_ type: FontType, size: CGFloat) -> UIFont? {
-        return .init(name: "Pretendard-\(type.rawValue)", size: size)
+    static func Pretendard(
+        type: FontType,
+        size: CGFloat
+    ) -> UIFont? {
+        return .init(
+            name: "Pretendard-\(type.rawValue)",
+            size: size
+        )
     }
 }
 
 extension UIFont {
-    static let head = UIFont.Pretendard(.bold, size: 20)
-    static let sub = UIFont.Pretendard(.bold, size: 14)
-    static let title = UIFont.Pretendard(.bold, size: 16)
-    static let body1 = UIFont.Pretendard(.medium, size: 16)
-    static let body2 = UIFont.Pretendard(.medium, size: 13)
-    static let caption = UIFont.Pretendard(.medium, size: 12)
-    static let button = UIFont.Pretendard(.bold, size: 15)
+    static let head = UIFont.Pretendard(
+        type: .bold,
+        size: 20
+    )
+    
+    static let sub = UIFont.Pretendard(
+        type: .bold,
+        size: 14
+    )
+    
+    static let title = UIFont.Pretendard(
+        type: .bold,
+        size: 16
+    )
+    
+    static let body1 = UIFont.Pretendard(
+        type: .medium,
+        size: 16
+    )
+    
+    static let body2 = UIFont.Pretendard(
+        type: .medium,
+        size: 13
+    )
+    
+    static let caption = UIFont.Pretendard(
+        type: .medium,
+        size: 12
+    )
+    
+    static let button = UIFont.Pretendard(
+        type: .bold,
+        size: 15
+    )
 }
