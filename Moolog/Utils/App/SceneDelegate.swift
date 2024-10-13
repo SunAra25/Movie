61cfbddf2e41ada745c6b422a6eca3e2b0a2f968
YeoSungeun <dutjddms@naver.com>
2024-10-14 07:50:56 +0900

    [Chore] rootView  변경 #17

--- Moolog/Utils/App/SceneDelegate.swift
+++ Moolog/Utils/App/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: scene)
         
-        window?.rootViewController = TrendViewController()
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
 
